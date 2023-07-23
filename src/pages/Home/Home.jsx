64d297e40d9e8c79ff065ebe8de0a6aec9a69523
mohammad <mehmandoost2@gmail.com>
2023-07-23 16:20:09 +0430

    fix responsive and add brands section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,29 +1,12 @@
 import React from "react";
-import "./Home.scss";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import tophome from "../../assets/tophome.png";
+import { Hero } from "../../components/Hero/Hero";
+import { Brand } from "../../components/Brand/Brand";
 
 export const Home = () => {
   return (
-    <div className="homecontain">
-      <div className="home">
-        <div className="left">
-
-            <h2>Collections</h2>
-            <p>
-              you can explore ans shop many differnt collection from various
-              barands here.
-            </p>
-            <div className="button">
-              <ShoppingCartIcon />
-              Shop Now
-            </div>
-
-        </div>
-        <div className="right">
-          <img src={tophome} alt="shop" />
-        </div>
-      </div>
+    <div className="totalhome">
+      <Hero />
+      <Brand />
     </div>
   );
 };
