a1888b6d457171602f347f7af3ea05de5f9c5b85
mohammad <mehmandoost2@gmail.com>
2023-07-23 17:49:16 +0430

    add home gallery

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Hero } from "../../components/Hero/Hero";
 import { Brand } from "../../components/Brand/Brand";
+import { Gallery } from "../../components/Gallery/Gallery";
 
 export const Home = () => {
   return (
     <div className="totalhome">
       <Hero />
       <Brand />
+      <Gallery />
     </div>
   );
 };
